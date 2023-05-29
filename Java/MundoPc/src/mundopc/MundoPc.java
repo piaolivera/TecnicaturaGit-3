3b6a27fa9f7b0391755125c8cdb83b66a847b7f6
piaolivera <piaoliveraa@gmail.com>
2023-05-29 16:42:32 -0300

    Creamos

--- Java/MundoPc/src/mundopc/MundoPc.java
+++ Java/MundoPc/src/mundopc/MundoPc.java
@@ -14,7 +14,7 @@ public class MundoPc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo ");
     }
     
 }
