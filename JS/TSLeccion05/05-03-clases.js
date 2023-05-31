148ef3ec24b80ed27a5151b57303b5b613b9631b
piaolivera <piaoliveraa@gmail.com>
2023-05-30 23:02:29 -0300

    Herencia en js

---
+++ JS/TSLeccion05/05-03-clases.js
@@ -0,0 +1,58 @@
+class Persona{ //Clase padre
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido=apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre (nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        return this._apellido= apellido;
+    }
+}
+
+class Empleado extends Persona{ //empleado clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento; 
+    }
+}
+
+let persona1 = new Persona('Martín', 'Perez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+//console.log(persona1);
+persona1.apellido = 'Lopez';
+console.log(persona1.apellido);
+
+let persona2= new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);
+persona2.nombre = 'Maria Laura';
+console.log(persona2.nombre);
+//console.log(persona2);
+persona2.apellido = 'Pereyra';
+console.log(persona2.apellido);
+
+
+let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombre);
+
+
+
+
+
